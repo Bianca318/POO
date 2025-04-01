e4cc2bacc8bd57c2a31d69669cc327d181098384
unknown <biancarupi30@gmail.com>
2025-04-01 16:02:39 +0300

    .

--- Gestionare_locuri_de_parcare/Gestionare-locuri-de-parcare.cpp
+++ Gestionare_locuri_de_parcare/Gestionare-locuri-de-parcare.cpp
@@ -14,7 +14,7 @@ protected: float oraintrarii;
          float tarif;
 
 public:
-    Parcare(float oraintrarii, float minutulintrarii, float oraiesirii, float minutuliesirii, int nrlocuriparcare,float tarif)
+    Parcare(float oraintrarii, float minutulintrarii, float oraiesirii, float minutuliesirii, int nrlocuriparcare, float tarif)
     {
         this->oraintrarii = oraintrarii;
         this->minutulintrarii = minutulintrarii;
@@ -23,42 +23,45 @@ public:
         this->nrlocuriparcare = nrlocuriparcare;
         this->tarif = tarif;
     }
-    
-    float calculeazatarif() const{ 
-        
-            float ora_totala, min_tot, min_total, tarif_final;
-            ora_totala = oraiesirii - oraintrarii;
-            min_tot = minutuliesirii - minutulintrarii;
-            min_total = ora_totala * 60 + min_tot;
-            tarif_final = min_total * (tarif/60);
 
-            return tarif_final;
-        }
+    float calculeazatarif() const {
+
+        float ora_totala, min_tot, min_total, tarif_final;
+        ora_totala = oraiesirii - oraintrarii;
+        min_tot = minutuliesirii - minutulintrarii;
+        min_total = ora_totala * 60 + min_tot;
+        tarif_final = min_total * (tarif / 60);
+
+        return tarif_final;
+    }
     void afisare_detalii() {
-        cout << "Ora intarii: " << oraintrarii << ", Minutul intarii:" << minutulintrarii << ", Ora iesirii: " << oraiesirii << ", Minutul iesirii:" << minutuliesirii << ", Numar locuri parcare:" << nrlocuriparcare << ", Tarif pe ora:" << calculeazatarif()<<"lei"<<endl;
+        cout << "Ora intarii: " << oraintrarii << ", Minutul intarii:" << minutulintrarii << ", Ora iesirii: " << oraiesirii << ", Minutul iesirii:" << minutuliesirii << endl;
+        cout << "Numar locuri parcare:" << nrlocuriparcare << ", Tarif:" << calculeazatarif() << "lei";
     }
 };
 class Masina : public Parcare {
-private: 
-       string numar;
+private:
+    string numar;
 public:
-    Masina(float oraintrarii,float minutulintrarii, float oraiesirii, float minutuliesirii, int nrlocuriparcare, float tarif, string numar) : Parcare(oraintrarii, minutulintrarii, oraiesirii, minutuliesirii, nrlocuriparcare, tarif)
+    Masina(float oraintrarii, float minutulintrarii, float oraiesirii, float minutuliesirii, int nrlocuriparcare, float tarif, string numar) : Parcare(oraintrarii, minutulintrarii, oraiesirii, minutuliesirii, nrlocuriparcare, tarif)
     {
         this->numar = numar;
     }
     void afisare_informatii()
     {
-        cout << "Ora intarii: " << oraintrarii  << ", Minutul intarii:" <<minutulintrarii<< ", Ora iesirii: " << oraiesirii << ", Minutul iesirii:" << minutuliesirii << ", Numar locuri parcare:" << nrlocuriparcare << ", Tarif pe ora:" << calculeazatarif() << ", Numar:" << numar;
+        cout << "Ora intarii: " << oraintrarii << ", Minutul intarii:" << minutulintrarii << ", Ora iesirii: " << oraiesirii << ", Minutul iesirii:" << minutuliesirii << endl;
+        cout << "Numar locuri parcare:" << nrlocuriparcare << ", Tarif:" << calculeazatarif() << ", Numar:" << numar;
     }
 
 };
 
 int main()
-{ int i,n;
-cout << "Afiseaza numarul:";
+{
+    int i, n;
+    cout << "Afiseaza numarul:";
     cin >> n;
 
-    Masina**masini=new Masina*[n];
+    Masina** masini = new Masina * [n];
 
     for (i = 0; i < n; i++)
     {
@@ -69,7 +72,7 @@ cout << "Afiseaza numarul:";
         int nrlocuriparcare;
         float tarif;
         string numar;
-        cout << "Masina " << i + 1 << " cu nr de inmatriculare: "; 
+        cout << "Masina " << i + 1 << " cu nr de inmatriculare: ";
         cin >> numar;
         cout << "Introduceti ora intrarii: ";
         cin >> oraintrarii;
@@ -79,26 +82,26 @@ cout << "Afiseaza numarul:";
         cin >> oraiesirii;
         cout << "Introduceti minutul iesirii:";
         cin >> minutuliesirii;
-        cout << "Introduceti numar locuri de parcare:"; 
+        cout << "Introduceti numar locuri de parcare:";
         cin >> nrlocuriparcare;
-        cout << "Tarif pe ora:";
+        cout << "Tarif:";
         cin >> tarif;
-       
 
-        masini[i] =new Masina(oraintrarii, minutulintrarii, oraiesirii, minutuliesirii, nrlocuriparcare, tarif, numar);
-        cout << "Total tarif:" << masini[i]->calculeazatarif() << "lei" << endl;
+
+        masini[i] = new Masina(oraintrarii, minutulintrarii, oraiesirii, minutuliesirii, nrlocuriparcare, tarif, numar);
+        cout << "Tarif:" << masini[i]->calculeazatarif() << "lei" << endl;
     }
 
-         for (i = 0; i < n; i++)
-         {
-             masini[i]->afisare_informatii();
-         }
+    for (i = 0; i < n; i++)
+    {
+        masini[i]->afisare_informatii();
+    }
 
-         for (i = 0; i < n; i++)
-         {
-           delete masini[i];
-         }
-         delete[] masini;
+    for (i = 0; i < n; i++)
+    {
+        delete masini[i];
+    }
+    delete[] masini;
 
 }
 
